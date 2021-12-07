9d4b7d8e1010c32db434b6da2268d4ece6d82f26
bbbbbr <bbbbbr@users.noreply.github.com>
2021-12-06 22:33:14 -0800

    Sprites: Ship: Switch to fixed point to improve movement speed (1.5 pixels per frame), turn off oam overflow guard

--- src/entity_ship.c
+++ src/entity_ship.c
@@ -17,14 +17,16 @@
 
 
 // TODO: optimize ship movement, probably fixed point and 1.5 pixels per frame
-#define SHIP_MOVE_AMT_X 1u
-#define SHIP_MOVE_AMT_Y 1u
+#define FIXED_BITSHIFT 8
+
+#define SHIP_MOVE_AMT_X (3u << (FIXED_BITSHIFT - 1)) // This comes out to 1.5 after fixed point conversion
+#define SHIP_MOVE_AMT_Y (3u << (FIXED_BITSHIFT - 1))
 
 #define SHIP_X_MIN (0u)
-#define SHIP_X_MAX (DEVICE_SCREEN_PX_WIDTH - sprite_ship_PIVOT_W)
+#define SHIP_X_MAX ((DEVICE_SCREEN_PX_WIDTH - sprite_ship_PIVOT_W) << FIXED_BITSHIFT)
 
-#define SHIP_Y_MIN (DEVICE_SCREEN_PX_HEIGHT * 3/5)
-#define SHIP_Y_MAX (DEVICE_SCREEN_PX_HEIGHT - (sprite_ship_PIVOT_H / 2u))
+#define SHIP_Y_MIN ((DEVICE_SCREEN_PX_HEIGHT * 3/5) << FIXED_BITSHIFT)
+#define SHIP_Y_MAX ((DEVICE_SCREEN_PX_HEIGHT - (sprite_ship_PIVOT_H / 2u)) << FIXED_BITSHIFT)
 
 #define SHIP_X_INIT (SHIP_X_MIN + ((SHIP_X_MAX - SHIP_X_MIN) / 2u))
 #define SHIP_Y_INIT (SHIP_Y_MIN + ((SHIP_Y_MAX - SHIP_Y_MIN) / 2u))
@@ -37,14 +39,13 @@
 #define SHIP_SPRITE_SEL_MAX       (4u << SHIP_SPRITE_SEL_BITSHIFT)
 #define SHIP_SPRITE_SEL_INIT      ((SHIP_SPRITE_SEL_MAX - SHIP_SPRITE_SEL_MIN) / 2)
 
-// TODO: 16 bit movement
-uint8_t ship_x, ship_y;
+fixed   ship_x, ship_y;
 uint8_t ship_sprite_sel;
 uint8_t ship_sprite_sel_temp;
 
 void entity_ship_init(void) {
-    ship_x = SHIP_X_INIT;
-    ship_y = SHIP_Y_INIT;
+    ship_x.w = SHIP_X_INIT;
+    ship_y.w = SHIP_Y_INIT;
 }
 
 void entity_ship_add(void) {
@@ -56,15 +57,15 @@ uint8_t entity_ship_update(uint8_t oam_high_water) {
     // TODO: map left/right panning
     // Horizontal movement
     if (KEY_PRESSED(J_LEFT)) {
-        if (ship_x > SHIP_X_MIN) {
-            ship_x -= SHIP_MOVE_AMT_X;
+        if (ship_x.w > SHIP_X_MIN) {
+            ship_x.w -= SHIP_MOVE_AMT_X;
         }
         if (ship_sprite_sel > SHIP_SPRITE_SEL_MIN)
             ship_sprite_sel--;
     }
     else if (KEY_PRESSED(J_RIGHT)) {
-        if (ship_x < SHIP_X_MAX) {
-            ship_x += SHIP_MOVE_AMT_X;
+        if (ship_x.w < SHIP_X_MAX) {
+            ship_x.w += SHIP_MOVE_AMT_X;
         }
         if (ship_sprite_sel < SHIP_SPRITE_SEL_MAX)
             ship_sprite_sel++;
@@ -77,34 +78,34 @@ uint8_t entity_ship_update(uint8_t oam_high_water) {
 
     // Vertical Movement
     if (KEY_PRESSED(J_UP)) {
-        if (ship_y > SHIP_Y_MIN) {
-            ship_y -= SHIP_MOVE_AMT_Y;
+        if (ship_y.w > SHIP_Y_MIN) {
+            ship_y.w -= SHIP_MOVE_AMT_Y;
         }
     }
     else if (KEY_PRESSED(J_DOWN)) {
-        if (ship_y < SHIP_Y_MAX) {
-            ship_y += SHIP_MOVE_AMT_Y;
+        if (ship_y.w < SHIP_Y_MAX) {
+            ship_y.w += SHIP_MOVE_AMT_Y;
         }
     }
 
     if (KEY_PRESSED(J_B)) {
-        entity_shots_add(ship_x, ship_y);
+        entity_shots_add(ship_x.h, ship_y.h);
     }
 
 
     // TODO: fixme
-    if (oam_high_water < (40 - 7 - 1)) { // TODO: defines here
+    // if (oam_high_water < (40 - 7 - 1)) { // TODO: defines here
 
         // Move ship and update metasprite
         oam_high_water += move_metasprite(sprite_ship_metasprites[(ship_sprite_sel >> SHIP_SPRITE_SEL_BITSHIFT)],
                                          (SPR_TILES_START_SHIP),
-                                         oam_high_water, ship_x, ship_y);
+                                         oam_high_water, ship_x.h, ship_y.h);
 
         // Canopy sprite on top of ship (1 x 8x16 sprite)
         oam_high_water += move_metasprite(sprite_ship_canopy_metasprites[(ship_sprite_sel >> SHIP_SPRITE_SEL_BITSHIFT)],
                                          (SPR_TILES_START_SHIP_CANOPY),
-                                         oam_high_water, ship_x + 8, ship_y); // TODO DEFINE X OFFSET
-    }
+                                         oam_high_water, ship_x.h + 8, ship_y.h); // TODO DEFINE X OFFSET
+    // }
 
     return (oam_high_water);
 }
